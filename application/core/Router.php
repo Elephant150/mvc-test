0933687bae4b01acea24485e2f7e18eccfe1c1ac
Elephant150 <dmytruk150@gmail.com>
2020-08-18 20:11:59 +0300

    added function for autoloading classes

---
+++ application/core/Router.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace application\core;
+
+class Router
+{
+    public function __construct()
+    {
+
+    }
+
+    public function add()
+    {
+
+    }
+
+    public function match()
+    {
+
+    }
+
+    public function start()
+    {
+
+    }
+}
--- index.php
+++ index.php
@@ -1,6 +1,15 @@
 <?php
 require_once 'application/lib/Dev.php';
-$arr = ['lol' ,4 , 12, 'kek'];
 
-debug($arr);
-echo 'lol';
+use application\lib\Test;
+
+spl_autoload_register(function ($class) {
+    $ds = DIRECTORY_SEPARATOR; //підбирає правильний слеш в різних ОС
+    $path = str_replace('\\', $ds, $class) . '.php'; //заміна зворотніх слешів на слеші, які використовує конкретна ОС
+
+    if (file_exists($path)) {
+        require $path;
+    }
+});
+
+$test = new Test();
